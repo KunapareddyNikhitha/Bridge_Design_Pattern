9d8a653ced474dec7e58efdf2f7af3c9ceded774
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-05-04 10:54:12 +0530

    Add project files.

---
+++ SamsungRemoteControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Design_Pattern
+{
+    public class SamsungRemoteControl : AbstractRemoteControl
+    {
+        public SamsungRemoteControl(ILEDTV ledTv)
+        {
+            this.ledTv = ledTv;
+        }
+        public override void SwitchOn()
+        {
+            ledTv.SwitchOn();
+        }
+        public override void SwitchOff()
+        {
+            ledTv.SwitchOff();
+        }
+        public override void SetChannel(int channelNumber)
+        {
+            ledTv.SetChannel(channelNumber);
+        }
+    }
+}
---
+++ SonyRemoteControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Design_Pattern
+{
+    // This is going to be a concrete class which inherits from the Abstraction class i.e. AbstractRemoteControl. 
+    // This Redefined Abstraction Class extends the interface defined by AbstractRemoteControl class.
+    public class SonyRemoteControl : AbstractRemoteControl
+    {
+        public SonyRemoteControl(ILEDTV ledTv)
+        {
+            this.ledTv = ledTv;
+        }
+        public override void SwitchOn()
+        {
+            ledTv.SwitchOn();
+        }
+        public override void SwitchOff()
+        {
+            ledTv.SwitchOff();
+        }
+        public override void SetChannel(int channelNumber)
+        {
+            ledTv.SetChannel(channelNumber);
+        }
+    }
+}
